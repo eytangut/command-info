e76896b5c92b1d888b1b18041616cc1b4c4e7df9
eytangut <73982765+eytangut@users.noreply.github.com>
2024-09-20 14:09:32 +0000

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Terminal from './components/Terminal';
 import ButtonInterface from './components/ButtonInterface';
 import CommandDetail from './components/CommandDetail';
@@ -7,7 +7,7 @@ import CommandDetail from './components/CommandDetail';
 // Main App component
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="min-h-screen bg-gray-900 text-green-500">
         <Routes>
           <Route path="/command-info/" element={<Terminal />} />
@@ -15,7 +15,7 @@ function App() {
           <Route path="/command-info/command/:name" element={<CommandDetail />} />
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
